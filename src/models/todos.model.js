e07d9df49aad770df047655d81dd3d26fffdc568
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-05-31 19:27:52 +0330

    feat(models): add category field to todos model
    Added a new field 'category' to the todos model with a string data type and not allowing null values.

--- src/models/todos.model.js
+++ src/models/todos.model.js
@@ -1,62 +1,66 @@
-const { Model, DataTypes } = require("sequelize");
+const {Model, DataTypes} = require("sequelize");
 
 module.exports = (sequelize) => {
-  class TodosModel extends Model {
-    static associate(models) {
-      TodosModel.belongsTo(models.users, {
-        foreignKey: "userId",
-        as: "users",
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-      });
+    class TodosModel extends Model {
+        static associate(models) {
+            TodosModel.belongsTo(models.users, {
+                foreignKey: "userId",
+                as: "users",
+                onDelete: "CASCADE",
+                onUpdate: "CASCADE",
+            });
+        }
     }
-  }
 
-  TodosModel.init(
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [3, 20],
+    TodosModel.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [3, 20],
+                },
+            },
+            description: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [3, 255],
+                },
+            },
+            status: {
+                type: DataTypes.ENUM("pending", "in-progress", "completed"),
+                allowNull: true,
+                defaultValue: "pending",
+            },
+            priority: {
+                type: DataTypes.ENUM("low", "medium", "high"),
+                allowNull: false,
+                defaultValue: "low",
+            },
+            category: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id",
+                    onDelete: "CASCADE",
+                    onUpdate: "CASCADE",
+                },
+            },
+            completedAt: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
         },
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-          len: [3, 255],
-        },
-      },
-      status: {
-        type: DataTypes.ENUM("pending", "in-progress", "completed"),
-        allowNull: true,
-        defaultValue: "pending",
-      },
-      priority: {
-        type: DataTypes.ENUM("low", "medium", "high"),
-        allowNull: false,
-        defaultValue: "low",
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "users",
-          key: "id",
-          onDelete: "CASCADE",
-          onUpdate: "CASCADE",
-        },
-      },
-      completedAt: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-    },
-    {
-      sequelize,
-      modelName: "todos",
-    }
-  );
-  return TodosModel;
+        {
+            sequelize,
+            modelName: "todos",
+        }
+    );
+    return TodosModel;
 };
